d8976b09b46a1663559ed418f5ea58e67ef79450
RadiatingPI46 <nzeluelijah@gmail.com>
2025-03-26 15:13:14 +0300

    Routing $ views

--- server/app.py
+++ server/app.py
@@ -6,3 +6,34 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:word>')
+def print_string(word):
+    print(word)
+    return word
+
+@app.route('/count/<int:integer>')
+def count(integer):
+    count = f''
+    for n in range(integer):
+        count += f'{n}\n'
+    return count
+
+@app.route('/math/<num1>/<operator>/<num2>')
+def math(num1,operator,num2):
+    if operator == '+':
+        return str(int(num1) + int(num2))
+    elif operator == '-':
+        return str(int(num1) - int(num2))
+    elif operator == '*':
+        return str(int(num1) * int(num2))
+    elif operator == 'div':
+        return str(int(num1) / int(num2))
+    elif operator == '%':
+        return str(int(num1) % int(num2))
+    else:
+        return 'Invalid operator'
